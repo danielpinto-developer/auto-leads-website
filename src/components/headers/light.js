67d10b522356ab28b0f34cd406708c1d7c2ea7eb
danielpinto-developer <danielpinto.developer@gmail.com>
2025-07-29 01:37:08 -0600

    added gray bg to header

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -3,7 +3,7 @@ import tw from "twin.macro";
 import styled from "styled-components";
 import logo from "../../images/logo.svg";
 
-const HeaderContainer = tw.header`bg-white w-full py-4 px-6 border-b border-gray-300`;
+const HeaderContainer = tw.header`bg-gray-200 w-full py-4 px-6 border-b border-gray-300`;
 const InnerContainer = tw.div`max-w-screen-xl mx-auto flex justify-between items-center`;
 
 const Logo = styled.img`
@@ -24,7 +24,7 @@ export default function SimpleHeader() {
       <InnerContainer>
         <Logo src={logo} alt="Confianza Total Logo" />
         <InfoBlock>
-          <PhoneHeading>El Seguro de Auto Que Te Mereces</PhoneHeading>
+          <PhoneHeading>El Seguro Que Te Mereces</PhoneHeading>
           <PhoneNumber href="tel:18885888415">1-888-588-8415</PhoneNumber>
         </InfoBlock>
       </InnerContainer>
